579996286eac9fa661333735933d247685a18777
Nurul Huda (Apon) <me@nurulhudaapon.com>
2025-11-04 01:16:24 +0600

    chore: downgrade next

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    viewTransition: true,
+  },
   images: {
     remotePatterns: [
       {
